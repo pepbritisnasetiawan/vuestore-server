9faffba5635b79c3a0c62cbdbada2d3e88d01c0f
pebritisnasetiawan <pebritisnasetiawan@gmail.com>
2024-10-15 22:07:27 +0700

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 8000;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
