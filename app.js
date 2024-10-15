b8c9c0b04d330008a3cdb14310154f7652ac3a70
pebritisnasetiawan <pebritisnasetiawan@gmail.com>
2024-10-15 18:53:04 +0700

    setup project, database and insert data dumy

--- app.js
+++ app.js
@@ -5,11 +5,27 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const db = require('./app/models');
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log('Database Connected!');
+  })
+  .catch((err) => {
+    console.log('Cannot connect to database!', err);
+    process.exit();
+  });
+
 app.get('/', (req, res) => {
   res.json({
     message: 'Welcome to vuestore server',
   });
 });
+
+require('./app/routes/product.route')(app)
 
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
